c437097821f8f5e5ea8a59e36af0a0bbe896ecd8
Cédric Dutoit <dutoitc@shimbawa.ch>
2015-06-19 16:21:31 +0200

    Small fixes

---
+++ src/main/java/ch/mno/talend/mitard/data/TBonitaInstanciateProcessType.java
@@ -0,0 +1,22 @@
+package ch.mno.talend.mitard.data;
+
+/**
+ * Created by dutoitc on 10.05.2015.
+ */
+public class TBonitaInstanciateProcessType extends AbstractNodeType {
+
+    private String processName;
+
+    @Override
+    public String toString() {
+        return "TJavaFlex["+getUniqueName()+"]";
+    }
+
+    public String getProcessName() {
+        return processName;
+    }
+
+    public void setProcessName(String processName) {
+        this.processName = processName;
+    }
+}
