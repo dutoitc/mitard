018242e1de500b219750bae1bc156726a2dfbe5a
Cédric Dutoit <dutoitc@shimbawa.ch>
2019-07-04 14:13:38 +0200

    Added violations count

--- src/main/resources/template/app/controllers/violations.js
+++ src/main/resources/template/app/controllers/violations.js
@@ -10,18 +10,44 @@ angular.module('mitardApp.violations', ['ngRoute'])
 
 .controller('ViolationsCtrl', ['$http', '$scope', function($http, $scope) {
 
+    $scope.stats={};
+
     $http.get('data/violations.json')
         .success(function(data) {
             for (var i=0, l=data["fileViolationses"].length; i<l; i++) {
                 var fileViolations = data["fileViolationses"][i];
-                var nbViolations = fileViolations["generalViolations"].length;
+
+                // General violations
+                var generalViolations = fileViolations["generalViolations"];
+                var nbViolations = generalViolations.length;
+                for (var j=0; j<generalViolations.length; j++) {
+                    var generalViolation = generalViolations[j];
+                    if ($scope.stats[generalViolation]) {
+                        $scope.stats[generalViolation]+=1;
+                    } else {
+                        $scope.stats[generalViolation]=1;
+                    }
+                }
+
+                // Component violations
                 var componentViolations = fileViolations["componentViolations"];
                 for (var cv in componentViolations) {
-                    nbViolations+=componentViolations[cv].length;
+                    var componentViolation = componentViolations[cv];
+                    nbViolations+=componentViolation.length;
+
+                    for (var vi=0; vi<componentViolation.length; vi++) {
+                        var violation = componentViolation[vi];
+                        if ($scope.stats[violation]) {
+                            $scope.stats[violation]+=1;
+                        } else {
+                            $scope.stats[violation]=1;
+                        }
+                    }
                 }
                 fileViolations.nbViolations=nbViolations;
             }
             $scope.violations=data;
+            console.log('Stats', $scope.stats);
         });
 
         $scope.showBubble = function(violation) {
